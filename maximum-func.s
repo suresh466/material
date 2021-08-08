ec2bec719e6309c5e6329e667e638e3d17bebdc4
assembly <assembly@assembly.con>
2021-08-08 22:58:54 +0545

    add ...maximum-func file

--- maximum-func.s
+++ maximum-func.s
@@ -8,10 +8,14 @@
 #	Result as status code.
 #
 #VARIABLES:
-#	% - Current result
+#	%edi - Holds index of data items.
+#	%eax - Holds current maximum.
+#	%ebx - Holds current data item.
+#	%ecx - Holds the initial address of data items.
 #
 #NOTE:
 #	Data item should be 1 or greater than 1.
+#	0 is used to signify end of data items.
 #
 
 .section .data
@@ -22,9 +26,28 @@
 
 .type maximum, @function
 maximum:
-	subl $4, %esp
-	movl %ebp, (%esp)
-	movl %esp, %ebp
-	
+	# Initialize the function.
+	subl $4, %esp	# Preserve old %ebp.
+	movl %ebp, (%esp)	#
+	movl %esp, %ebp	# Make %ebp equal to %esp.
+	movl 8(%ebp), %ecx	# Pointer to data items.
 
+	movl $0, %edi	# Index for the data items.
+	movl (%ecx), %eax	# Since it is the first number.
+
+	maximum_loop_start:
+		incl %edi
+		movl (%ecx, %edi, 4), %ebx # Load next data item.
+		cmpl $0, %ebx	# Check to see if we hit the end.
+		je maximum_loop_end
+
+		cmpl %ebx, %eax
+		jge maximum_loop_start
+		movl %ebx, %eax
+		jmp maximum_loop_start
 	
+	maximum_loop_end:
+		movl %ebp, %esp
+		movl (%esp), %ebp
+		addl $4, %esp
+		ret
