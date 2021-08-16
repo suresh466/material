baa37819a183db4b26259cb58f66d68ccea1f754
assembly <assembly@assembly.con>
2021-08-16 16:19:43 +0545

    fix indentation of largest-age program

--- unit6/usetheconcepts/largest-age.s
+++ unit6/usetheconcepts/largest-age.s
@@ -32,23 +32,23 @@ _start:
 	movl %eax, ST_DESCRIPTOR(%ebp)
 
 	start_loop:
-	pushl ST_DESCRIPTOR(%ebp)
-	pushl $read_buffer
-	call read_record
-	addl $8, %esp
+		pushl ST_DESCRIPTOR(%ebp)
+		pushl $read_buffer
+		call read_record
+		addl $8, %esp
 
-	cmpl $RECORD_SIZE, %eax
-	jne end_loop
+		cmpl $RECORD_SIZE, %eax
+		jne end_loop
 
-	movl $RECORD_AGE+read_buffer, %eax	#Load the address of age.
-	movl (%eax), %eax	#Load the age.
-	cmpl largest_age, %eax
-	jle start_loop
-	movl %eax, largest_age
-	jmp start_loop
+		movl $RECORD_AGE+read_buffer, %eax	#Load the address of age.
+		movl (%eax), %eax	#Load the age.
+		cmpl largest_age, %eax
+		jle start_loop
+		movl %eax, largest_age
+		jmp start_loop
 
 	end_loop:
-	movl %ebp, %esp
-	movl $SYS_EXIT, %eax
-	movl largest_age, %ebx
-	int $LINUX_SYSCALL
+		movl %ebp, %esp
+		movl $SYS_EXIT, %eax
+		movl largest_age, %ebx
+		int $LINUX_SYSCALL
