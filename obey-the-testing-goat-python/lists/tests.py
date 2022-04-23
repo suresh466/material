ed56986b713ccc0c423dc7c950f8b4405b13fadb
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-04-23 12:03:52 +0545

    Refactor home page view to use a template

--- obey-the-testing-goat-python/lists/tests.py
+++ obey-the-testing-goat-python/lists/tests.py
@@ -8,14 +8,8 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        template = 'lists/home.html'
+        self.assertTemplateUsed(response, template)
--- obey-the-testing-goat-python/lists/views.py
+++ obey-the-testing-goat-python/lists/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do</title></html>')
+    template = 'lists/home.html'
+    return render(request, template)
