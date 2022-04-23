0c20e54d99e637f03823091bb40a278e15728330
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-04-23 10:51:41 +0545

    Functional test check, we can input a to-do item

--- obey-the-testing-goat-python/functional_tests.py
+++ obey-the-testing-goat-python/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,17 +17,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices that the title of the app says todo app.
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a todo item right away.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She entered "Buy peacock feathers" into a text box
+        # (her hobby is tying fly-fishing lures).
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter the page updates and the entry is displayed as:
+        # "1:  Buy peacock feathers" as an item in the todo list.
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. She enters
+        # "Use peacock feathers to make a fly" (Edith is very methodical).
         self.fail('Finish the test!')
-
-# She is invited to enter a todo item right away.
-
-# She entered "Buy peacock feathers" into a text box (her hobby is tying fly-fishing lures).
-
-# When she hits enter the page updates and the entry is displayed as:
-# "1:  Buy peacock feathers" as an item in the todo list.
-
-# There is still a text box inviting her to add another item. She enters
-# "Use peacock feathers to make a fly" (Edith is very methodical).
 
 # The site updates again and now shows both items on the list.
 
