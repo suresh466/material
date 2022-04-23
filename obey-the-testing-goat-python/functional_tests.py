af3d065e306c3ad95cc5609e76f61487486972ae
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-04-23 15:38:37 +0545

    Refactor functional test, use a helper function

--- obey-the-testing-goat-python/functional_tests.py
+++ obey-the-testing-goat-python/functional_tests.py
@@ -8,6 +8,11 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def tearDown(self):
         self.browser.quit()
 
@@ -35,12 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         # "1:  Buy peacock feathers" as an item in the todo list.
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows]
-        )
+        row_text = '1: Buy peacock feathers'
+        self.check_for_row_in_list_table(row_text)
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly" (Edith is very methodical).
@@ -50,16 +51,11 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The site updates again and now shows both items on the list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows]
-        )
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        row_text = '1: Buy peacock feathers'
+        self.check_for_row_in_list_table(row_text)
+        row_text = '2: Use peacock feathers to make a fly'
+        self.check_for_row_in_list_table(row_text)
+
         self.fail('Finish the test!')
 
 # She wonders if the website remembers her todo items and notices the website has
