7077b10852343269947da96f2997c21233eb44d0
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-04-24 16:15:24 +0545

    add new URL, view and template to display lists

--- obey-the-testing-goat-python/lists/tests.py
+++ obey-the-testing-goat-python/lists/tests.py
@@ -26,17 +26,10 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 class ItemModelTest(TestCase):
+
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
@@ -57,3 +50,20 @@ class ItemModelTest(TestCase):
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        template = 'lists/list.html'
+        self.assertTemplateUsed(response, template)
+
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemy 1')
+        Item.objects.create(text='itemy 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemy 1')
+        self.assertContains(response, 'itemy 2')
--- obey-the-testing-goat-python/lists/views.py
+++ obey-the-testing-goat-python/lists/views.py
@@ -11,7 +11,11 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, template)
+
+def view_list(request):
+    template = 'lists/list.html'
 
     items = Item.objects.all()
     context = {'items': items}
