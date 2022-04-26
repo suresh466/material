58c8e474f3c2d2f77844c29c28ab5ef62204d814
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-04-26 16:26:14 +0545

    get FT ready for staging server

--- obey-the-testing-goat-python/functional_tests/tests.py
+++ obey-the-testing-goat-python/functional_tests/tests.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+import os
 
 MAX_WAIT = 10
 
@@ -11,6 +12,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
